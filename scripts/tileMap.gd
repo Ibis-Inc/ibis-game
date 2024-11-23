d2e10e315d785af59f5d87d79b6f4fa725d6b3c8
AppleSheep <95598768+appleshoops@users.noreply.github.com>
2024-11-23 16:22:41 +1100

    weFIJIOJRAFD

---
+++ scripts/tileMap.gd
@@ -0,0 +1,27 @@
+extends TileMap
+
+var stupid = FastNoiseLite.new()
+var stupid2  = FastNoiseLite.new()
+var stupid3 = FastNoiseLite.new()
+
+var width = 32
+var height = 32
+
+func _ready() -> void:
+	stupid.seed = randi()
+	stupid2.seed = randi()
+	stupid3.seed = randi()
+
+
+func _process(delta: float) -> void:
+	generate_chunk(position)
+
+
+func generate_chunk(position):
+	var tile_pos = local_to_map(position)
+	for x in range(width):
+		for y in range(height):
+			var stupo = stupid.get_noise_2d(tile_pos.x + x, tile_pos.y + y)
+			var stupo2 = stupid2.get_noise_2d(tile_pos.x + x, tile_pos.y + y)
+			var stupo3 = stupid3.get_noise_2d(tile_pos.x + x, tile_pos.y + y)
+			set_cell(0, Vector2i(tile_pos.x + x, tile_pos.y + y), 0, Vector2i(1, 1))
