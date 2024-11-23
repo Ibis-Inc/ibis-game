555a3bf41375cdc8c022fd4c9b1b70f9758497c4
Coosanta <coosantamapy@gmail.com>
2024-11-23 14:14:51 +1100

    yes
    yesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyesyes

--- player.gd
+++ player.gd
@@ -3,6 +3,8 @@ extends Area2D
 @export var speed = 200
 var screen_size
 
+var scale_h
+
 func _ready():
 	screen_size = get_viewport_rect().size
 
@@ -13,7 +15,7 @@ func _process(delta):
 	if Input.is_action_pressed("move_left"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+		velocity.y += 1
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -24,11 +26,20 @@ func _process(delta):
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 
+	if velocity.x < 0:
+		scale_h = -1
+	
+	if velocity.x > 0:
+		scale_h = 1
+
 	if velocity.x !=0:
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_v = false
-		$AnimatedSprite2D.flip_h = velocity.x < 0
+		# $AnimatedSprite2D.scale.x = velocity.x
+		$AnimatedSprite2D.scale.x = scale_h
 	if velocity.x == 0:
 		$AnimatedSprite2D.animation = "stand still"
 		$AnimatedSprite2D.flip_v = false
 		$AnimatedSprite2D.flip_h = false
+	if velocity.y > 0:
+		$AnimatedSprite2D.animation = "jump"
