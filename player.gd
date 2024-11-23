c614509f9b0b3eda5d3214a47c63b70da44aef51
AppleSheep <95598768+appleshoops@users.noreply.github.com>
2024-11-23 14:00:16 +1100

    fix thing

--- player.gd
+++ player.gd
@@ -9,9 +9,9 @@ func _ready():
 func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 
